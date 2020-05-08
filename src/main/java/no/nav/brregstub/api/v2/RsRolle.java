3038da673c081001e354d711e961fa398ccfaec1
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2020-05-08 09:25:12 +0200

    REG-8156: Fikset swagger-beskrivelser

--- src/main/java/no/nav/brregstub/api/v2/RsRolle.java
+++ src/main/java/no/nav/brregstub/api/v2/RsRolle.java
@@ -19,7 +19,7 @@ public class RsRolle {
     @NotNull
     private LocalDate registreringsdato;
 
-    @ApiModelProperty(example = "Innehaver", required = true)
+    @ApiModelProperty(dataType = "java.lang.Enum", example = "INNH", required = true)
     @NotBlank
     private RolleKode rollebeskrivelse;
 
