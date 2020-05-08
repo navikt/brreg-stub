01ff3c8549d5ffd1fccadf9bb0993e5745b94a54
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2020-05-08 08:22:49 +0200

    REG-8156: La til @Data og @NoArgs på RsRolleStatus

--- src/main/java/no/nav/brregstub/api/v2/RsRolleStatus.java
+++ src/main/java/no/nav/brregstub/api/v2/RsRolleStatus.java
@@ -1,11 +1,15 @@
 package no.nav.brregstub.api.v2;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 import no.nav.brregstub.api.RolleKode;
 
+@Data
+@NoArgsConstructor
 public class RsRolleStatus {
 
     @ApiModelProperty(dataType = "java.lang.String", example = "DAGL", required = true)
